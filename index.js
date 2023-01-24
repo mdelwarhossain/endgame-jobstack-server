21b0b96d821df6ceb19dbbf3bf696e130e392e7c
MRPrio <maksudurprio.mp@gmail.com>
2023-01-24 13:09:22 +0600

    commit

--- index.js
+++ index.js
@@ -91,24 +91,7 @@ async function run() {
       res.send(result);
     });
 
-    app.put("/updatelike/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: ObjectId(id) };
-      const like = req.body;
-      console.log(like);
-      const option = { upsert: true };
-      const updatedlike = {
-        $set: {
-          likes: like.like,
-        },
-      };
-      const result = await postsCollection.updateOne(
-        filter,
-        updatedlike,
-        option
-      );
-      res.send(result);
-    });
+
 
     //comepoents collection
     app.post("/comments", async (req, res) => {
