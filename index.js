b8cc6c19cfcff7e1db8ec7d0e176ec0455c2cd09
mddelwarhossain <dhsaeed88@gmail.com>
2023-02-15 18:43:44 +0100

    resume route commented

--- index.js
+++ index.js
@@ -641,6 +641,7 @@ async function run() {
     //get a individual user by email
     app.get("/user/:email", async (req, res) => {
       const email = req.params.email;
+      console.log(email);
       const query = { email: email };
       const user = await usersCollection.findOne(query);
       // console.log(user);
