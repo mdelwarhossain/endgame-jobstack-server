99f85a7419044a3df791e454fbc8afd5f943641b
MRPrio <maksudurprio.mp@gmail.com>
2023-02-17 14:34:41 +0600

    added

--- index.js
+++ index.js
@@ -16,11 +16,19 @@ const app = express();
 // app.use(cors());
 app.use(express.json());
 
-app.use(cors({
-  origin:"*",
-  methods:["GET","POST","PUT","DELETE"]
+// app.use(cors({
+//   origin:"*",
+//   methods:["GET","POST","PUT","DELETE"]
 
-}))
+// }))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Max-Age', '3600'); // Set to 1 hour
+  next();
+});
+
 
 
 
